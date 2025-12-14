cb85a7a94c9816d89edfd38628c31a95935d32bd
Shahriear AL Amin <rion7213@gmail.com>
2025-12-14 17:02:44 +0600

    New E Commerce

---
+++ src/app/Header/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <header className=' h-[60px] bg-[red]'>
+            <div className='container'>
+                <nav className=''>
+                    <h1 className=' text-[red]'></h1>
+                </nav>
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '@/app/Header/page'
+const page = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default page
