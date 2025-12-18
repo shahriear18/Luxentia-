4afaf4cc478419c0bfc61e8d591798f853d86ff8
Shahriear AL Amin <rion7213@gmail.com>
2025-12-18 20:34:22 +0600

    Day 4 evening..

--- src/app/Starlink/page.jsx
+++ src/app/Starlink/page.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import StarlinkCart from '../../../Strlinkcart.jsx/page'
 const page = () => {
   return (
-    <div>page</div>
+    <div className=' container'>
+      <div className=' border border-b-1 border-t-0 border-l-0 border-r-0 py-[20px] border-[#c0bebe]'>
+        <h1 className=' font-[600] text-[25px] text-[#0404be]'>Starlink Prise in Bangladesh</h1>
+        <h2 className=' text-[13px] mt-[10px]'>Starlink Price starts from BDT 23,850 to BDT 185,000 in Bangladesh depending on hardware and package. Buy latest Starlink Hardware Kit officially from Star Tech online shop. Browse below and order yours now!</h2>
+      </div>
+      <div className=' flex'>
+        <div className=' mt-[30px] w-[250px] border-[#a3a1a1]' >
+          <h3 className=' mb-[20px] text-[16px] font-[600]'>Price Range</h3>
+          <div className=' flex gap-3 items-center'>
+            <div>
+            <input className=' flex justify-center border-[#a7a4a4] w-[80px] px-[5px] outline-0 border border-r-0  border-l-0'  type="number" placeholder=' 940' />
+          </div>
+          <p>To</p>
+          <div>
+            <input className=' flex justify-center border-[#a7a4a4] w-[80px] px-[5px] outline-0 border border-r-0  border-l-0'  type="number" placeholder='18,9000' />
+          </div>            
+          </div>
+          <button className=' font-[600] bg-[#ff7b00] px-[40px] py-[5px] rounded-[20px] mt-[15px] flex justify-center ml-[40px]'>Filter</button>
+          <div className=' mt-[30px]'>
+            <p className=' font-[600] mb-[10px] text-[17px]'>Avilability</p>
+            <ul className=' grid gap-1'>
+              <li className=' text-[#292828] text-[15px] font-[600] flex items-center gap-2'><input type="checkbox" /> In Stack </li>
+              <li className=' text-[#292828] text-[15px] font-[600] flex items-center gap-2'><input type="checkbox" /> Pre order </li>
+              <li className=' text-[#292828] text-[15px] font-[600] flex items-center gap-2'><input type="checkbox" /> Up Coming </li>
+            </ul>
+          </div>
+        </div>
+        <StarlinkCart/>
+      </div>
+    </div>
   )
 }
 
