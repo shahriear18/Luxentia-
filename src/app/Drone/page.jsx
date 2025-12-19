abfae100dfc19b69f54023fd3c29a7f4be360fe0
Shahriear AL Amin <rion7213@gmail.com>
2025-12-20 01:39:37 +0600

    Day 6 night..

--- src/app/Drone/page.jsx
+++ src/app/Drone/page.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
-
+'use client'
+import React, { useState } from 'react'
+import { FaAngleUp } from "react-icons/fa6";
+import Dronecart from '../../../Droncart/page'
 const page = () => {
+  let [option,setoption] = useState(true);
+  let [option2,setoption2] = useState(true);
+  let [option3,setoption3] = useState(true);
+  let [option4,setoption4] = useState(true);
   return (
-    <div>page</div>
+    <div className=' container'>
+      <div className=' border border-b-1 border-t-0 border-l-0 border-r-0 py-[20px] border-[#c0bebe]'>
+        <h1 className=' font-[600] text-[25px] text-[#0404be]'>Drone Price in Bangladesh</h1>
+        <h2 className=' text-[13px] mt-[10px]'>Drone Price in Bangladesh starts from BDT 5,250 and depending on the features and brand the price may go up to BDT 385,000, At Star Tech you can get the latest Drones from popular brands like DJI. Browse below and order yours now!</h2>
+      </div>
+      <div className=' flex'>
+        <div className=' mt-[30px] w-[250px] border-[#a3a1a1]' >
+          <h3 className=' mb-[20px] text-[16px] font-[600]'>Price Range</h3>
+          <div className=' flex gap-3 items-center'>
+            <div>
+            <input className=' flex justify-center border-[#a7a4a4] w-[80px] px-[5px] outline-0 border border-r-0  border-l-0'  type="number" placeholder=' 940' />
+          </div>
+          <p>To</p>
+          <div>
+            <input className=' flex justify-center border-[#a7a4a4] w-[80px] px-[5px] outline-0 border border-r-0  border-l-0'  type="number" placeholder='18,9000' />
+          </div>            
+          </div>
+          <button className=' font-[600] bg-[#ff7b00] px-[40px] py-[5px] rounded-[20px] mt-[15px] flex justify-center ml-[40px]'>Filter</button>
+          <div className=' mt-[30px]'>
+            <div className=' flex justify-between w-[190px]'>
+            <p className='  font-[600] mb-[15px] text-[17px]'>Avilability</p>
+             <FaAngleUp style={{rotate : option? " 180deg":"0deg"}} onClick={() =>{setoption(!option)}} className=' transition-all' />
+            </div>
+            <ul style={{ height : option? "120px" : '0px'}} className=' grid gap-3 transition-all overflow-hidden'>
+              <li className=' text-[#292828] text-[15px] font-[600] flex items-center gap-2'><input type="checkbox" /> In Stack </li>
+              <li className=' text-[#292828] text-[15px] font-[600] flex items-center gap-2'><input type="checkbox" /> Pre order </li>
+              <li className=' text-[#292828] text-[15px] font-[600] flex items-center gap-2'><input type="checkbox" /> Up Coming </li>
+            </ul>
+          </div>
+        </div>
+        <Dronecart/>
+      </div>
+    </div>
   )
 }
 
